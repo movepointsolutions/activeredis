86f549595568d3cf7c2abf5b2986c40c7c59e32e
Anek <movepoint@yandex.ru>
2023-08-28 11:52:08 +0500

    works

--- test_sync.cpp
+++ test_sync.cpp
@@ -4,24 +4,32 @@
 #include "algorithm.h++"
 #include "model.h++"
 
-struct model_type
-	: public teach::model<std::string_view, std::string_view>
-{
-	void emit(const std::string_view &k, const std::string_view &v)
-	{
-		std::cout << v << " (" << k << ")" << std::endl;
-	}
-};
-
-typedef teach::algorithm<std::unique_ptr<model_type>> algorithm_type;
-algorithm_type algorithm(std::unique_ptr<model_type>(new model_type()));
-
 class db : public ar::db
 {
 public:
+	struct model_type
+		: public teach::model<std::string_view, std::string_view>
+		, public std::enable_shared_from_this<model_type>
+	{
+		void emit(const std::string_view &k,
+		          const std::string_view &v)
+		{
+			std::cout << v << " (" << k << ")" << std::endl;
+		}
+	};
+
+	typedef std::shared_ptr<model_type> mptr_type;
+	typedef teach::algorithm<mptr_type> algorithm_type;
+
+	mptr_type model;
+	algorithm_type algorithm;
+
 	//using seventh database at localhost
 	//standard port
-	db() : ar::db("redis://127.0.0.1:6379/7") { }
+	db() : ar::db("redis://127.0.0.1:6379/7")
+	     , model{new model_type()}
+	     , algorithm(model)
+	{ }
 
 	void handle_key(std::string_view key)
 	{
