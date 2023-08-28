002472492aa3dcadc623775ce69ca520c1d12d62
Anek <movepoint@yandex.ru>
2023-08-28 11:31:10 +0500

    works

--- test_sync.cpp
+++ test_sync.cpp
@@ -4,26 +4,37 @@
 #include "algorithm.h++"
 #include "model.h++"
 
-struct model_type : public teach::model<std::string, std::string>
+struct model_type
+	: public teach::model<std::string_view, std::string_view>
 {
-	void emit(const std::string &k, const std::string &v)
+	void emit(const std::string_view &k, const std::string_view &v)
 	{
-		std::cout << k << ", " << v << "!" << std::endl;
+		std::cout << v << " (" << k << ")" << std::endl;
 	}
 };
 
 typedef teach::algorithm<std::unique_ptr<model_type>> algorithm_type;
 algorithm_type algorithm(std::unique_ptr<model_type>(new model_type()));
 
+class db : public ar::db
+{
+public:
+	//using seventh database at localhost
+	//standard port
+	db() : ar::db("redis://127.0.0.1:6379/7") { }
+
+	void handle_key(std::string_view key)
+	{
+		std::string_view k{key};
+		std::string v = this->get(k);
+		algorithm.transport(k, v);
+	}
+};
+
 void
 test1()
 {
-	//using seventh database at localhost
-	//standard port
-	ar::db db("redis://127.0.0.1:6379/7");
-	auto key = "hello";
-	auto value = db.get(key);
-	algorithm.transport(key, value);
+	db().handle_key("world");
 }
 
 int
